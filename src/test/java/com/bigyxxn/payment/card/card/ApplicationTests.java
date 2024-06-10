8692e9e92965f529779504d5329aedc4dff8984a
bigyxxn <bigyxxn@gmail.com>
2024-06-10 20:59:13 +0900

    Initial commit

---
+++ src/test/java/com/bigyxxn/payment/card/card/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigyxxn.payment.card.card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
